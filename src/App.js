b5b36dd0d490701b657db6d9b9831a1cc8c12e2c
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-13 22:10:15 +0600

    create google button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 
 import './App.css';
+import {getAuth, GoogleAuthProvider} from 'firebase/auth'
+import app from './Firebase/firebase.init';
+
+const auth = getAuth(app);
 
 function App() {
+  const provider = new GoogleAuthProvider();
+
+  const handleGoogleSignIn = () => {
+    console.log("google")
+  }
   return (
     <div className="App">
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
       
     </div>
   );
--- src/__Steps.js
+++ src/__Steps.js
@@ -5,5 +5,11 @@
 4,Register web app > firebase project home > click web > give name > register
 5, install firebase for your project : npm install firebase
 6,Dangerous : get firebase config and put it in firebase.init.js
-7.Do not expose your firebase config
+7.Export app from firebase.init.js
+8,Create auth using getAuth from firebase by using app from firebase.init.js
+9,create a google auth provider , do not forget to use new google auth provider new GoogleAuthProvider();
+10, go to firebase > build > authentication > sign in method
+11, enable google sign in method
+12,create a button for google sign in method with a click handler.
+13,
 */
