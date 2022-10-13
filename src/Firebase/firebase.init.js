431c36f0bad199020092619da788efabc1f663a5
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-13 21:45:10 +0600

    initial commit

---
+++ src/Firebase/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDt1aYEpFgbVr_SG1cjf2ehDzbdArSCaG4",
+  authDomain: "simple-firebase-authenti-66daf.firebaseapp.com",
+  projectId: "simple-firebase-authenti-66daf",
+  storageBucket: "simple-firebase-authenti-66daf.appspot.com",
+  messagingSenderId: "649795068319",
+  appId: "1:649795068319:web:429b45a16a7ade9f2cddaa"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
---
+++ src/__Steps.js
@@ -0,0 +1,9 @@
+/*
+1,Visit : console.firebase.google.com
+2,create a new firebase projects
+3,visit doc(go to doc):Build > authentication > web > Getting started.
+4,Register web app > firebase project home > click web > give name > register
+5, install firebase for your project : npm install firebase
+6,Dangerous : get firebase config and put it in firebase.init.js
+7.Do not expose your firebase config
+*/
